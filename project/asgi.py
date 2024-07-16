bf7ae3fb8723e20f34716a48bccea36f44ef0f0c
Akshit Chauhan <159024855+Akshit3022@users.noreply.github.com>
2024-07-16 11:05:06 +0530

    removed asgi error

--- project/asgi.py
+++ project/asgi.py
@@ -7,24 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
 
-# project/asgi.py
-
-# project/asgi.py
 
 import os
+
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-# import exercise.routing  # Import your app's routing module
-# from exercise.routing import websocket_urlpatterns  # Import your WebSocket URL patterns
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings")
 
-# application = ProtocolTypeRouter({
-#     "http": get_asgi_application(),
-#     "websocket": AuthMiddlewareStack(
-#         URLRouter(
-#             websocket_urlpatterns
-#         )
-#     ),
-# })
+application = get_asgi_application()
